6f61aa4f5bfeaa0cdd72fcb77c002b8b4e07cee6
Auxence-M <auxencemedja@gmail.com>
2025-04-15 21:03:59 -0400

    Added the city command

--- api/weather.go
+++ api/weather.go
@@ -8,11 +8,14 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"text/tabwriter"
+	"time"
 )
 
 var apiKey string
 
-type CurrentWeather struct {
+type WeatherData struct {
 	Coordinates struct {
 		Lon float64 `json:"lon"`
 		Lat float64 `json:"lat"`
@@ -75,6 +78,7 @@ type CurrentWeather struct {
 	Message  string `json:"message"`
 }
 
+// Reads config file and sets the api key
 func initConfig() {
 	viper.SetConfigName("config")
 	viper.AddConfigPath("./config")
@@ -88,7 +92,8 @@ func initConfig() {
 	apiKey = viper.GetString("OPEN_WEATHER_MAP_API_KEY")
 }
 
-func GetWeatherByCity(city string, country string, units string) (CurrentWeather, error) {
+// GetWeatherByCity Collects weather data using a city name
+func GetWeatherByCity(city string, country string, units string) (WeatherData, error) {
 
 	// Constructing the api url using city name
 	apiUrl := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s,%s&units=%s&APPID=%s", city, country, units, apiKey)
@@ -96,32 +101,33 @@ func GetWeatherByCity(city string, country string, units string) (CurrentWeather
 	// Http get request to open weather api
 	response, err := http.Get(apiUrl)
 	if err != nil {
-		return CurrentWeather{}, err
+		return WeatherData{}, err
 	}
 	defer response.Body.Close()
 
 	// Reading the response body
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		return CurrentWeather{}, err
+		return WeatherData{}, err
 	}
 
 	// Parsing JSON response to a currentWeather object
-	var currentWeather CurrentWeather
+	var currentWeather WeatherData
 	err = json.Unmarshal(body, &currentWeather)
 	if err != nil {
-		return CurrentWeather{}, err
+		return WeatherData{}, err
 	}
 
 	// Handle specific bad request errors
 	if currentWeather.Code != 200 {
-		return CurrentWeather{}, errors.New(currentWeather.Message)
+		return WeatherData{}, errors.New(currentWeather.Message)
 	}
 
 	return currentWeather, nil
 }
 
-func GetWeatherByZip(zipCode string, country string, units string) (CurrentWeather, error) {
+// GetWeatherByZip Collects weather data using a zipcode
+func GetWeatherByZip(zipCode string, country string, units string) (WeatherData, error) {
 
 	// Constructing the api url using postal code
 	apiUrl := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?zip=%s,%s&units=%s&APPID=%s", zipCode, country, units, apiKey)
@@ -129,24 +135,94 @@ func GetWeatherByZip(zipCode string, country string, units string) (CurrentWeath
 	// Http get request to open weather api
 	response, err := http.Get(apiUrl)
 	if err != nil {
-		return CurrentWeather{}, err
+		return WeatherData{}, err
 	}
 	defer response.Body.Close()
 
 	// Reading the response body
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		return CurrentWeather{}, err
+		return WeatherData{}, err
 	}
 
 	// Parsing JSON response to a currentWeather object
-	var currentWeather CurrentWeather
+	var currentWeather WeatherData
 	err = json.Unmarshal(body, &currentWeather)
 	if err != nil {
-		return CurrentWeather{}, err
+		return WeatherData{}, err
 	}
 
 	return currentWeather, nil
+}
+
+func PrintWeatherData(currentWeather WeatherData, detailed bool, units string) {
+
+	// Create a tabwriter instance.
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
+
+	fmt.Println("Here is the current Condition data.")
+	fmt.Fprintln(w, "City:\t", currentWeather.Name, currentWeather.Sys.Country)
+	fmt.Fprintln(w, "Temperature:\t", currentWeather.Main.Temp, printTemp(units))
+	fmt.Fprintln(w, "Feels like:\t", currentWeather.Main.FeelsLike, printTemp(units))
+	fmt.Fprintln(w, "Min Temperature:\t", currentWeather.Main.TempMin, printTemp(units))
+	fmt.Fprintln(w, "Max Temperature:\t", currentWeather.Main.TempMax, printTemp(units))
+	fmt.Fprintln(w, "Condition:\t", currentWeather.Weather[0].Description)
+
+	if detailed {
+		fmt.Fprintln(w, "Humidity:\t", currentWeather.Main.Humidity, "%")
+		fmt.Fprintln(w, "Pressure :\t", currentWeather.Main.Pressure, "hPa")
+		fmt.Fprintln(w, "Cloudiness :\t", currentWeather.Clouds.All, "%")
+		fmt.Fprintln(w, "Wind speed :\t", currentWeather.Wind.Speed, printSpeed(units))
+		fmt.Fprintln(w, "Wind direction :\t", currentWeather.Wind.Deg, "%")
+		fmt.Fprintln(w, "Wind gust :\t", currentWeather.Wind.Gust, printSpeed(units))
+		fmt.Fprintln(w, "Sunrise:\t", time.Unix(int64(currentWeather.Sys.Sunrise), 0).Format(time.TimeOnly))
+		fmt.Fprintln(w, "Sunset:\t", time.Unix(int64(currentWeather.Sys.Sunset), 0).Format(time.TimeOnly))
+		fmt.Fprintln(w, "Longitude:\t", currentWeather.Coordinates.Lon)
+		fmt.Fprintln(w, "Latitude:\t", currentWeather.Coordinates.Lat)
+	}
+
+	fmt.Fprintln(w, "Time of data collection:\t", time.Unix(int64(currentWeather.Dt), 0).Format(time.DateTime))
+
+	err := w.Flush()
+	if err != nil {
+		return
+	}
+}
+
+// GetUnits Convert units flag value into the correct metric value
+func GetUnits(flag string) string {
+	switch flag {
+	case "S":
+		return "standard"
+	case "I":
+		return "imperial"
+	default:
+		return "metric"
+	}
+}
+
+// Print temperature symbols based on unit system
+func printSpeed(units string) string {
+	switch units {
+	case "S":
+		return "m/s"
+	case "I":
+		return "mph"
+	default:
+		return "m/s"
+	}
+}
+
+// Print speed symbols based on unit system
+func printTemp(units string) string {
+	switch units {
+	case "S":
+		return "K"
+	case "I":
+		return "°F"
+	default:
+		return "°C"
+	}
 }
 
 func init() {
